6256dd6c87507727d1deca32c906e9cf9e893df8
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-06-27 17:20:21 +0200

    add(ferrilata-devilline): add api/pitches endpoint

--- ferrilata-devilline/Controllers/ApiController.cs
+++ ferrilata-devilline/Controllers/ApiController.cs
@@ -1,3 +1,26 @@
-﻿namespace ferrilata_devilline.Controllers
+﻿using System.Linq;
+using ferrilata_devilline.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ferrilata_devilline.Controllers
 {
+    public class RestController : Controller
+    {
+        [Route("api/pitches")]
+        public JsonResult Return_Pitches()
+        {
+            var re = Request;
+
+            var headers = re.Headers;
+            if (headers.ContainsKey("Authorization") && (headers.First(h => h.Key == "Authorization").Value != "" ||
+                                                         !(headers.First(h => h.Key == "Authorization").Value
+                                                             .ToString() is null)))
+                return Json(new Pitches());
+            else
+            {
+                Response.StatusCode = 401;
+                return Json(new Error("Unauthorizied"));
+            }
+        }
+    }
 }
---
+++ ferrilata-devilline/Models/Error.cs
@@ -0,0 +1,12 @@
+namespace ferrilata_devilline.Models
+{
+    public class Error
+    {
+        public string error { get; set; }
+
+        public Error(string error)
+        {
+            this.error = error;
+        }
+    }
+}
