e142b48d90693142255b7e7c8c9e517479c81b76
Zsófi Gagyi <gagyizsofi@gmail.com>
2019-06-27 21:38:53 +0200

    feat: return Created or Unauthorized in function of "Authorization"

--- ferrilata-devilline/Controllers/ApiController.cs
+++ ferrilata-devilline/Controllers/ApiController.cs
@@ -1,3 +1,29 @@
-﻿namespace ferrilata_devilline.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace ferrilata_devilline.Controllers
 {
+    [ApiController]
+    public class ApiController : Controller
+    {
+        [HttpPost("/api/add-assignee")]
+        public IActionResult AddAssignee([FromHeader] string Authorization, [FromBody]JObject data)
+        {
+            if (Authorization != null && Authorization != "")
+            { 
+                var messageObject = new { message = "Created" };
+                List<object> responseList = new List<object>();
+                responseList.Add(messageObject);
+
+                var result = new ObjectResult(responseList);
+                result.StatusCode = 201;
+                return result;
+            }
+            else
+            {
+                return Unauthorized( Json( new { error = "Unauthorized" }));
+            }
+        }
+    }
 }
