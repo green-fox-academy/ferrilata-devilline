c0bcfc3474649b05a06d95acfca1dda54f2a3759
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-06-27 17:43:15 +0200

    test(IntegrationTests): test api/pitches endpoint status returns 401

--- ferrilata-devilline.IntegrationTests/Scenarios/IntegrationTests.cs
+++ ferrilata-devilline.IntegrationTests/Scenarios/IntegrationTests.cs
@@ -1,4 +1,10 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using ferrilata_devilline.IntegrationTests.Fixtures;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace ferrilata_devilline.IntegrationTests.Scenarios
@@ -12,5 +18,20 @@ namespace ferrilata_devilline.IntegrationTests.Scenarios
         {
             _testContext = testContext;
         }
+
+        [Theory]
+        [InlineData("/api/pitches")]
+        public async Task PitchesApi_Should_Return401_WhenRequestHeaderAuthorizationIsMissing(string url)
+        {
+            //Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            //Act
+            var response = await _testContext.Client.SendAsync(request);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+        
     }
 }
