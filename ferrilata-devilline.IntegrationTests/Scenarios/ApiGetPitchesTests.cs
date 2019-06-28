65eddcefff28dc73ef5fc85516300c9f5b7a77cc
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-06-28 09:39:48 +0200

    test(IntegrationTests): test api/pitches endpoint status OK, JSON response

---
+++ ferrilata-devilline.IntegrationTests/Scenarios/ApiGetPitchesTests.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ferrilata_devilline.IntegrationTests.Fixtures;
+using ferrilata_devilline.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace ferrilata_devilline.IntegrationTests.Scenarios
+{
+    [Collection("BaseCollection")]
+    public class IntegrationTests
+    {
+        private readonly TestContext _testContext;
+
+        public IntegrationTests(TestContext testContext)
+        {
+            _testContext = testContext;
+        }
+
+        [Theory]
+        [InlineData("/api/pitches")]
+        public async Task PitchesApi_Should_Return401_WhenRequestHeaderAuthorizationIsMissing(string url)
+        {
+            //Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+            //Act
+            var response = await _testContext.Client.SendAsync(request);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("/api/pitches")]
+        public async Task PitchesApi_Should_ReturnErrorMessage_WhenRequestHeaderAuthorizationIsMissing(string url)
+        {
+            //Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var expected = new Error("Unauthorizied");
+
+            //Act
+            var response = await _testContext.Client.SendAsync(request);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var actual = JsonConvert.DeserializeObject<Error>(responseString);
+
+            //Assert
+            Assert.True(actual.Equals(expected));
+        }
+
+        [Theory]
+        [InlineData("/api/pitches")]
+        public async Task PitchesApi_Should_ReturnStatusOK_WhenRequestHeaderAuthorizationIsNotEmpty(string url)
+        {
+            //Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Add("Authorization", "something");
+
+            //Act
+            var response = await _testContext.Client.SendAsync(request);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("/api/pitches")]
+        public async Task PitchesApi_Should_ReturnJSONPitchers_WhenRequestHeaderAuthorizationIsNotEmpty(string url)
+        {
+            //Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Add("Authorization", "something");
+
+            //Act
+            var response = await _testContext.Client.SendAsync(request);
+
+            //Assert
+            var responseString = await response.Content.ReadAsStringAsync();
+            var actual = JsonConvert.DeserializeObject<Pitches>(responseString);
+            Assert.True((actual.GetType() == typeof(Pitches)));
+        }
+    }
+}
--- ferrilata-devilline/Models/Error.cs
+++ ferrilata-devilline/Models/Error.cs
@@ -8,5 +8,33 @@ namespace ferrilata_devilline.Models
         {
             this.error = error;
         }
+
+        protected bool Equals(Error other)
+        {
+            return string.Equals(error, other.error);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Error) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (error != null ? error.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Error left, Error right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Error left, Error right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
