d4aefb5bc87893c6fa6358c441c0981344891a44
Anna Sabransky <anna.sabransky@gmail.com>
2019-06-28 09:15:56 +0200

    feat(FD-4 Api Controller): authorization, successful and unsuccessful response added

--- ferrilata-devilline/Controllers/ApiController.cs
+++ ferrilata-devilline/Controllers/ApiController.cs
@@ -1,3 +1,35 @@
-﻿namespace ferrilata_devilline.Controllers
+﻿using ferrilata_devilline.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ferrilata_devilline.Controllers
 {
+    public class ApiController : Controller
+    {
+        [HttpGet]
+        [Route("/api/badges")]
+        public IActionResult getGadgets()
+        {
+            var request = Request;
+            var headers = request.Headers;
+
+            if ((headers.ContainsKey("Authorization")) && (headers["Autharization"].ToString() != null))
+            
+            { 
+                return Ok(new BadgesBase());
+            }
+
+            return Unauthorized(new { error = "Unauthorized" });
+
+        }
+    }
+
+   
+
+    
 }
---
+++ ferrilata-devilline/Models/BadgeBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ferrilata_devilline.Models
+{
+    public class BadgesBase
+    {
+        public List<Badge> Badges { get; set; }
+
+        public BadgesBase()
+        {
+            Badges = new List<Badge>();
+            Badges.Add(new Badge { Name = "Process improve", Level = 2 });
+            Badges.Add(new Badge { Name = "English speaker", Level = 1 });
+            Badges.Add(new Badge { Name = "Feedback giver", Level = 1 });
+        }
+
+    }
+}
