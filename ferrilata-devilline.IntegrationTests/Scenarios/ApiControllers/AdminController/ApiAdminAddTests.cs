ac7e7aad8923fb7f1883106123e8227d0aa0ecf1
Zsófi Gagyi <gagyizsofi@gmail.com>
2019-06-30 12:58:05 +0200

    refactor(ApiAdminController post): eliminate code duplication from test

--- ferrilata-devilline.IntegrationTests/Scenarios/ApiControllers/AdminController/ApiAdminAddTests.cs
+++ ferrilata-devilline.IntegrationTests/Scenarios/ApiControllers/AdminController/ApiAdminAddTests.cs
@@ -14,135 +14,120 @@ namespace ferrilata_devilline.IntegrationTests.Scenarios
     public class ApiAdminAddTests
     {
         private readonly TestContext _testContext;
-        private readonly HttpContent CorrectRequestContent; 
-        private readonly HttpContent RequestContentWithMissingFields;
-        private readonly HttpContent RequestContentWithNullValues;
+        private readonly HttpRequestMessage _message;
 
         public ApiAdminAddTests(TestContext testContext)
         {
             _testContext = testContext;
-
-            var CorrectRequestBodyObject = new  { version = "2.3", name = "Badge inserter", tag = "general", levels = new List<object>() };
-            string correctRequestBody = JsonConvert.SerializeObject(CorrectRequestBodyObject);
-            this.CorrectRequestContent = new StringContent(correctRequestBody, Encoding.UTF8, "application/json");
-
-            var IncorrectRequestBodyObject = new { version = "2.3", levels = new List<object>() };
-            string IncorrectRequestBody = JsonConvert.SerializeObject(IncorrectRequestBodyObject);
-            this.RequestContentWithMissingFields = new StringContent(IncorrectRequestBody, Encoding.UTF8, "application/json");
-
-            var IncorrectRequestBodyObject2 = new AdminDTO { version = null, name = "Badge inserter", tag = "general", levels = new List<object>() };
-            string IncorrectRequestBody2 = JsonConvert.SerializeObject(IncorrectRequestBodyObject2);
-            this.RequestContentWithNullValues = new StringContent(IncorrectRequestBody2, Encoding.UTF8, "application/json");
+            _message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
         }
 
-        [Fact]
-        public async Task Authorized_AndHasCorrectBody_Created()
-        {
-            var message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
-            message.Headers.Add("Authorization", "something");
-            message.Content = CorrectRequestContent;
+        #region test body setup
+        public static IEnumerable<object[]> MissingFields =>
+            new List<object[]>()
+            {
+                new object[] { new StringContent(JsonConvert.SerializeObject(
+                    new { version = "2.3", levels = new List<object>() }), 
+                    Encoding.UTF8, "application/json") },
+            };
 
-            var response = await _testContext.Client.SendAsync(message);
+        public static IEnumerable<object[]> Correct =>
+            new List<object[]>()
+            {
+                new object[] { new StringContent(JsonConvert.SerializeObject(
+                    new { version = "2.3", name = "Badge inserter", tag = "general", levels = new List<object>() }),
+                    Encoding.UTF8, "application/json") },
+            };
+
+        public static IEnumerable<object[]> NullValue =>
+            new List<object[]>()
+            {
+                new object[] { new StringContent(JsonConvert.SerializeObject(
+                    new AdminDTO { version = null, name = "Badge inserter", tag = "general", levels = new List<object>() }), 
+                    Encoding.UTF8, "application/json")},
+            };
+        #endregion
+
+        [Theory]
+        [MemberData(nameof(Correct))]
+        public async Task Authorized_AndHasCorrectBody_Created(HttpContent content)
+        {
+            _message.Headers.Add("Authorization", "something");
+            _message.Content = content;
+
+            var response = await _testContext.Client.SendAsync(_message);
 
             Assert.Equal(HttpStatusCode.Created, response.StatusCode);
         }
 
-        [Fact]
-        public async Task Authorized_AndHasCorrectBody_ResponseObject()
+        [Theory]
+        [MemberData(nameof(Correct))]
+        public async Task Authorized_AndHasCorrectBody_ResponseObject(HttpContent content)
         {
             var expectedResponseObject = new List<object>() { new { message = "Created" } };
             string expectedResponseString = JsonConvert.SerializeObject(expectedResponseObject);
             HttpContent expectedResponse = new StringContent(expectedResponseString, Encoding.UTF8, "application/json");
             string expected = await expectedResponse.ReadAsStringAsync();
 
-            var message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
-            message.Headers.Add("Authorization", "something");
-            message.Content = CorrectRequestContent;
+            _message.Headers.Add("Authorization", "something");
+            _message.Content = content;
 
-            var response = await _testContext.Client.SendAsync(message);
+            var response = await _testContext.Client.SendAsync(_message);
             string received = await response.Content.ReadAsStringAsync();
 
             Assert.Equal(expected, received);
         }
 
-        [Fact]
-        public async Task AuthorizationFieldIsEmpty_Unauthorized()
+        [Theory]
+        [MemberData(nameof(Correct))]
+        public async Task AuthorizationFieldIsEmpty_Unauthorized(HttpContent content)
         {
-            var message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
-            message.Headers.TryAddWithoutValidation("Authorization", "");
-            message.Content = RequestContentWithNullValues;
+            _message.Headers.TryAddWithoutValidation("Authorization", "");
+            _message.Content = content;
 
-            var response = await _testContext.Client.SendAsync(message);
+            var response = await _testContext.Client.SendAsync(_message);
 
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
 
-        [Fact]
-        public async Task AuthorizationFieldIsMissing_Unauthorized()
+        [Theory]
+        [MemberData(nameof(Correct))]
+        public async Task AuthorizationFieldIsMissing_Unauthorized(HttpContent content)
         {
-            var message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
-            message.Content = RequestContentWithNullValues;
+            _message.Content = content;
 
-            var response = await _testContext.Client.SendAsync(message);
+            var response = await _testContext.Client.SendAsync(_message);
 
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
 
-        [Fact]
-        public async Task Authorized_AndBodyHasNullValues_BadRequest()
+        [Theory]
+        [MemberData(nameof(MissingFields))]
+        [MemberData(nameof(NullValue))]
+        public async Task Authorized_IncorrectBody_BadRequest(HttpContent content)
         {
-            var message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
-            message.Headers.Add("Authorization", "something");
-            message.Content = RequestContentWithNullValues;
+            _message.Headers.Add("Authorization", "something");
+            _message.Content = content;
 
-            var response = await _testContext.Client.SendAsync(message);
+            var response = await _testContext.Client.SendAsync(_message);
 
-            Assert.Equal("BadRequest", response.StatusCode.ToString());
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
 
-        [Fact]
-        public async Task Authorized_AndBodyHasNullValues_ResponseObject()
+        [Theory]
+        [MemberData(nameof(MissingFields))]
+        [MemberData(nameof(NullValue))]
+        public async Task Authorized_IncorrectBody_ResponseObject(HttpContent content)
         {
             var expectedResponseObject = new { error = "Please provide all fields" };
             string expectedResponseString = JsonConvert.SerializeObject(expectedResponseObject);
             HttpContent expectedResponse = new StringContent(expectedResponseString, Encoding.UTF8, "application/json");
             string expected = await expectedResponse.ReadAsStringAsync();
 
-            var message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
-            message.Headers.Add("Authorization", "something");
-            message.Content = RequestContentWithNullValues;
+            _message.Headers.Add("Authorization", "something");
+            _message.Content = content;
 
-            var response = await _testContext.Client.SendAsync(message);
-            string received = await response.Content.ReadAsStringAsync();
-
-            Assert.Equal(expected, received);
-        }
-
-        [Fact]
-        public async Task Authorized_AndBodyHasMissingFields_BadRequest()
-        {
-            var message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
-            message.Headers.Add("Authorization", "something");
-            message.Content = RequestContentWithMissingFields;
-
-            var response = await _testContext.Client.SendAsync(message);
-
-            Assert.Equal("BadRequest", response.StatusCode.ToString());
-        }
-
-        [Fact]
-        public async Task Authorized_AndBodyHasMissingFields_ResponseObject()
-        {
-            var expectedResponseObject = new { error = "Please provide all fields" };
-            string expectedResponseString = JsonConvert.SerializeObject(expectedResponseObject);
-            HttpContent expectedResponse = new StringContent(expectedResponseString, Encoding.UTF8, "application/json");
-            string expected = await expectedResponse.ReadAsStringAsync();
-
-            var message = new HttpRequestMessage(HttpMethod.Post, "/api/admin/add");
-            message.Headers.Add("Authorization", "something");
-            message.Content = RequestContentWithMissingFields;
-
-            var response = await _testContext.Client.SendAsync(message);
+            var response = await _testContext.Client.SendAsync(_message);
             string received = await response.Content.ReadAsStringAsync();
 
             Assert.Equal(expected, received);
