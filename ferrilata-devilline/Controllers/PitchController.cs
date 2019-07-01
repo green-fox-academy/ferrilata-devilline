94230de8adb656692e6c74f061b4c52304170d64
Iaroslav <iaroslav.miller@gmail.com>
2019-07-01 14:03:53 +0200

    feat(PitchController): style changes following discussion with Suvi

--- ferrilata-devilline/Controllers/PitchController.cs
+++ ferrilata-devilline/Controllers/PitchController.cs
@@ -1,5 +1,4 @@
 ﻿using ferrilata_devilline.Models;
-using ferrilata_devilline.Models.ErrorModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ferrilata_devilline.Controllers
@@ -8,8 +7,6 @@ namespace ferrilata_devilline.Controllers
     [Route("api")]
     public class PitchController : Controller
     {
-
-
         public PitchController()
         {
         }
@@ -20,18 +17,23 @@ namespace ferrilata_devilline.Controllers
             var request = Request;
             var headers = request.Headers;
 
-            if (!headers.ContainsKey("Authorization")
-                || headers["Authorization"].ToString().Length == 0) return Unauthorized(new { message = "Unauthorized" });
+            if (!headers.ContainsKey("Authorization") ||
+                headers["Authorization"].ToString().Length == 0)
+            {
+                return Unauthorized(new { message = "Unauthorized" });
+            }
 
 
-            if (NewPitch == null
-                || NewPitch.BadgeName == null
-                || NewPitch.Holders == null
-                || NewPitch.OldLVL == 0
-                || NewPitch.PitchedLVL == 0
-                || NewPitch.PitchMessage == null) return NotFound(new { error = "Please provide all fields" });
-
-            return Ok(new { message = "Created" });
+            if (NewPitch == null ||
+                NewPitch.BadgeName == null ||
+                NewPitch.Holders == null ||
+                NewPitch.OldLVL == 0 ||
+                NewPitch.PitchedLVL == 0 ||
+                NewPitch.PitchMessage == null)
+            {
+                return NotFound(new { error = "Please provide all fields" });
+            }
+            return Json(new { message = "Created" });
         }
     }
 }
