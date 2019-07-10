c02c3626d386a7758e0e9a382955b66e7ebeb456
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-07-10 19:42:22 +0200

    fix(misc): move .sln file to root folder

--- ferrilata-devilline.IntegrationTests/Fixtures/HtmlReader.cs
+++ ferrilata-devilline.IntegrationTests/Fixtures/HtmlReader.cs
@@ -15,14 +15,14 @@ namespace ferrilata_devilline.IntegrationTests.Fixtures
         {
             var content = await response.Content.ReadAsStringAsync();
             Action<VirtualResponse> action = x => x.Address(response.RequestMessage.RequestUri)
-                                                    .Status(response.StatusCode)
-                                                    .Headers(response.Headers)
-                                                    .Headers(response.Content.Headers)
-                                                    .Content(content);
+                .Status(response.StatusCode)
+                .Headers(response.Headers)
+                .Headers(response.Content.Headers)
+                .Content(content);
 
             var document = await BrowsingContext.New()
                 .OpenAsync(action, CancellationToken.None);
-            return (IHtmlDocument)document;
+            return (IHtmlDocument) document;
         }
     }
 }
