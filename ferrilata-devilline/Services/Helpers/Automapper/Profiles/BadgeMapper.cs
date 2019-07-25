8580cf69311aa0974bddc1839356e96a92bddc92
Iaroslav <iaroslav.miller@gmail.com>
2019-07-25 21:45:25 +0200

    continue automapper setup

---
+++ ferrilata-devilline/Services/Helpers/Automapper/Profiles/BadgeMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AutoMapper;
+using ferrilata_devilline.Models.DAOs;
+using ferrilata_devilline.Models.DTOs;
+
+namespace ferrilata_devilline.Services.Helpers.AutoMapper.Profiles
+{
+    public class BadgeMapper : Profile
+    {
+        public BadgeMapper()
+        {
+            CreateMap<BadgeInDTO, Badge>();
+            CreateMap<LevelInDTO, Level>();
+        }
+    }
+}
--- ferrilata-devilline/Startup.cs
+++ ferrilata-devilline/Startup.cs
@@ -15,92 +15,100 @@ using System;
 
 namespace ferrilata_devilline
 {
-    public class Startup
-    {
-        private IConfiguration Configuration;
+	public class Startup
+	{
+		private IConfiguration Configuration;
 
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddAuthentication(options =>
-                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie()
-                .AddGoogle(options =>
-                {
-                    IConfigurationSection googleAuthNSection =
-                        Configuration.GetSection("Authentication:Google");
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddAuthentication(options =>
+					options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme)
+				.AddCookie()
+				.AddGoogle(options =>
+				{
+					IConfigurationSection googleAuthNSection =
+						Configuration.GetSection("Authentication:Google");
 
-                    options.ClientId = googleAuthNSection["ClientId"];
-                    options.ClientSecret = googleAuthNSection["ClientSecret"];
-                });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+					options.ClientId = googleAuthNSection["ClientId"];
+					options.ClientSecret = googleAuthNSection["ClientSecret"];
+				});
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddDbContext<ApplicationContext>(builder => builder
-                .UseMySQL($"server={Environment.GetEnvironmentVariable("FDHOST")}; " +
-                          $"database={Environment.GetEnvironmentVariable("FDDATABASE")}; " +
-                          $"user={Environment.GetEnvironmentVariable("FDUSERNAME")};" +
-                          $" password={Environment.GetEnvironmentVariable("FDPASSWORD")};"));
+			services.AddDbContext<ApplicationContext>(builder => builder
+				.UseMySQL($"server={Environment.GetEnvironmentVariable("FDHOST")}; " +
+						  $"database={Environment.GetEnvironmentVariable("FDDATABASE")}; " +
+						  $"user={Environment.GetEnvironmentVariable("FDUSERNAME")};" +
+						  $" password={Environment.GetEnvironmentVariable("FDPASSWORD")};"));
 
-            services.Configure<SlackOptions>(Configuration.GetSection("SlackOptions"));
-            services.AddScoped<IBadgeRepository, BadgeRepository>();
-            services.AddScoped<IBadgeService, BadgeService>();
-            services.AddScoped<IPitchService, MockPitchService>();
-            services.AddScoped<ISlackMessagingService, SlackMessagingService>();
+			services.Configure<SlackOptions>(Configuration.GetSection("SlackOptions"));
+			services.AddScoped<IBadgeRepository, BadgeRepository>();
+			services.AddScoped<IBadgeService, BadgeService>();
+			services.AddScoped<IPitchService, MockPitchService>();
+			services.AddScoped<ISlackMessagingService, SlackMessagingService>();
 
-            var currentlyUsedContext = services
-                .BuildServiceProvider()
-                .GetRequiredService<ApplicationContext>();
-            currentlyUsedContext.SeedWithData();
-        }
+			var currentlyUsedContext = services
+				.BuildServiceProvider()
+				.GetRequiredService<ApplicationContext>();
+			currentlyUsedContext.SeedWithData();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			services.SetUpAutoMapper();
 
-            app.UseStaticFiles();
-            app.UseMvc();
-            app.UseAuthentication();
-        }
 
-        public void ConfigureProductionServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+		}
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            services.AddDbContext<ApplicationContext>(builder => builder.UseInMemoryDatabase("InMemory"));
-        }
+			app.UseStaticFiles();
+			app.UseMvc();
+			app.UseAuthentication();
+		}
 
-        public void ConfigureTestingServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+		public void ConfigureProductionServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddDbContext<ApplicationContext>(builder => builder.UseInMemoryDatabase("InMemory"), ServiceLifetime.Singleton);
-            services.AddScoped<IBadgeRepository, BadgeRepository>();
-            services.AddScoped<IBadgeService, BadgeService>();
-            services.AddScoped<IPitchService, MockPitchService>();
-        }
-    }
+			services.AddDbContext<ApplicationContext>(builder => builder.UseInMemoryDatabase("InMemory"));
+
+			services.SetUpAutoMapper();
+		}
+
+		public void ConfigureTestingServices(IServiceCollection services)
+		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+			services.AddDbContext<ApplicationContext>(builder => builder.UseInMemoryDatabase("InMemory"), ServiceLifetime.Singleton);
+			services.AddScoped<IBadgeRepository, BadgeRepository>();
+			services.AddScoped<IBadgeService, BadgeService>();
+			services.AddScoped<IPitchService, MockPitchService>();
+
+			services.SetUpAutoMapper();
+		}
+	}
 }
