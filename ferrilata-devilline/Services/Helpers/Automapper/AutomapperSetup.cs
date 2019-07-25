2b577ef93be12d93c7da20242e98749f1b986f9c
Iaroslav <iaroslav.miller@gmail.com>
2019-07-25 21:30:01 +0200

    add Automapper setup

---
+++ ferrilata-devilline/Services/Helpers/Automapper/AutomapperSetup.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ferrilata_devilline.Services.Helpers.Automapper
+{
+    public class AutomapperSetup
+    {
+        public AutomapperSetup()
+        {
+        }
+    }
+}
