6b1bda0fbb9c09b4d900ce8c3a0d033f9a15ef95
Iaroslav <iaroslav.miller@gmail.com>
2019-07-25 17:09:49 +0200

    FD-38 commit

---
+++ ferrilata-devilline/Migrations/20190725144240_Initial1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ferrilata_devilline.Migrations
+{
+    public partial class Initial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Levels_Badges_hello",
+                table: "Levels");
+
+            migrationBuilder.RenameColumn(
+                name: "hello",
+                table: "Levels",
+                newName: "f");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Levels_hello",
+                table: "Levels",
+                newName: "IX_Levels_f");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Levels_Badges_f",
+                table: "Levels",
+                column: "f",
+                principalTable: "Badges",
+                principalColumn: "BadgeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Levels_Badges_f",
+                table: "Levels");
+
+            migrationBuilder.RenameColumn(
+                name: "f",
+                table: "Levels",
+                newName: "hello");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Levels_f",
+                table: "Levels",
+                newName: "IX_Levels_hello");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Levels_Badges_hello",
+                table: "Levels",
+                column: "hello",
+                principalTable: "Badges",
+                principalColumn: "BadgeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ ferrilata-devilline/Migrations/20190725144311_Initial2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ferrilata_devilline.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Levels_Badges_f",
+                table: "Levels");
+
+            migrationBuilder.RenameColumn(
+                name: "f",
+                table: "Levels",
+                newName: "BadgeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Levels_f",
+                table: "Levels",
+                newName: "IX_Levels_BadgeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Levels_Badges_BadgeId",
+                table: "Levels",
+                column: "BadgeId",
+                principalTable: "Badges",
+                principalColumn: "BadgeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Levels_Badges_BadgeId",
+                table: "Levels");
+
+            migrationBuilder.RenameColumn(
+                name: "BadgeId",
+                table: "Levels",
+                newName: "f");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Levels_BadgeId",
+                table: "Levels",
+                newName: "IX_Levels_f");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Levels_Badges_f",
+                table: "Levels",
+                column: "f",
+                principalTable: "Badges",
+                principalColumn: "BadgeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
