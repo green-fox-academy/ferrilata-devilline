3a96d753ec57a04fea0e2f19e4bf4126c3ec8617
Ksenia Kulikova <kskulikovaa@gmail.com>
2019-07-29 14:39:47 +0200

    test(BadgeController): test cascade deletion (related objects) when deleting badge by id

--- ferrilata-devilline.IntegrationTests/Scenarios/ApiControllers/Badges/ApiBadgesTests.cs
+++ ferrilata-devilline.IntegrationTests/Scenarios/ApiControllers/Badges/ApiBadgesTests.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using AngleSharp.Common;
+using ferrilata_devilline.Models.DAOs;
 using ferrilata_devilline.Models.DTOs;
 using Xunit;
 
@@ -85,6 +87,61 @@ namespace ferrilata_devilline.IntegrationTests
             await testContext.Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
             Assert.True(testContext.Context.Badges.Count(badge => badge.BadgeId == 1) == 0);
         }
-        
+
+        [Fact]
+        public async Task DeleteBadgeApi_CorrectAuthentication_ShouldDeleteLevelsAssociatedWithBadge()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, "/api/badges/1");
+            request.Headers.Add("Authorization", "test");
+            var levels = testContext.Context.Badges.GetItemByIndex(0).Levels;
+            await testContext.Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            for (var i = 0; i < levels.Count; i++)
+            {
+                Assert.Empty(testContext.Context.Levels.Where(l => l.LevelId == levels[i].LevelId));
+            }
+        }
+
+        [Fact]
+        public async Task DeleteBadgeApi_CorrectAuthentication_ShouldDeletePitchAssociatedWithBadge()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, "/api/badges/1");
+            request.Headers.Add("Authorization", "test");
+            var levels = testContext.Context.Badges.GetItemByIndex(0).Levels;
+            var pitches = levels.SelectMany(l => l.Pitches).ToList();
+            await testContext.Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            for (var i = 0; i < pitches.Count; i++)
+            {
+                Assert.Empty(testContext.Context.Pitches.Where(p => p.PitchId == pitches[i].PitchId));
+            }
+        }
+
+        [Fact]
+        public async Task DeleteBadgeApi_CorrectAuthentication_ShouldDeleteUserLevelAssociatedWithBadge()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, "/api/badges/1");
+            request.Headers.Add("Authorization", "test");
+            var levels = testContext.Context.Badges.GetItemByIndex(0).Levels;
+            var userLevels = levels.SelectMany(l => l.UserLevels).ToList();
+            await testContext.Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            for (var i = 0; i < userLevels.Count; i++)
+            {
+                Assert.Empty(testContext.Context.UserLevels.Where(ul => ul.LevelId == userLevels[i].LevelId));
+            }
+        }
+
+        [Fact]
+        public async Task DeleteBadgeApi_CorrectAuthentication_ShouldDeleteReviewAssociatedWithBadge()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, "/api/badges/1");
+            request.Headers.Add("Authorization", "test");
+            var levels = testContext.Context.Badges.GetItemByIndex(0).Levels;
+            var pitches = levels.SelectMany(l => l.Pitches).ToList();
+            var reviews = pitches.SelectMany(p => p.Reviews).ToList();
+            await testContext.Client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            for (var i = 0; i < reviews.Count; i++)
+            {
+                Assert.Empty(testContext.Context.Reviews.Where(r => r.ReviewId == reviews[i].ReviewId));
+            }
+        }
     }
 }
