a0229bbe21547310af70921cddcde66a51dd87f9
Anna <anna.sabransky@gmail.com>
2019-08-19 18:26:45 +0200

    layout mypitches link added

---
+++ ferrilata-devilline/Controllers/ViewControllers/MyPitchesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ferrilata_devilline.Controllers.ViewControllers
+{
+    [Authorize(AuthenticationSchemes = GoogleDefaults.AuthenticationScheme)]
+    public class MyPitchesController : Controller
+    {
+        [HttpGet("/mypitches")]
+        public IActionResult getMyPitches()
+        {
+            return View();
+        }
+    }
+}
