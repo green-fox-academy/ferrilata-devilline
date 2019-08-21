68e873825b94282764374dc11062f499ff4e1a2c
Anna <anna.sabransky@gmail.com>
2019-08-21 12:48:48 +0200

    UserService added

--- ferrilata-devilline/Services/Interfaces/IUserService.cs
+++ ferrilata-devilline/Services/Interfaces/IUserService.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using ferrilata_devilline.Models.DAOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ferrilata_devilline.Services.Interfaces
 {
-    interface IUserService
+    public interface IUserService
     {
+        User FindById(long id);
+        List<User> GetAll();
+        void Add(User user);
+        void DeleteById(long id);
+        void Update();
     }
 }
---
+++ ferrilata-devilline/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using ferrilata_devilline.Models.DAOs;
+using ferrilata_devilline.Repositories;
+using ferrilata_devilline.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace ferrilata_devilline.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User FindById(long id)
+        {
+            return _userRepository.FindUserById(id);
+        }
+        public void Add(User user)
+        {
+            _userRepository.SaveUser(user);
+        }
+
+        public void DeleteById(long id)
+        {
+            _userRepository.DeleteUsereById(id);
+        }
+
+        public List<User> GetAll()
+        {
+            return _userRepository.RetrieveUsersFromDB();
+        }
+
+        public void Update()
+        {
+            _userRepository.Update();
+        }
+    }
+}
